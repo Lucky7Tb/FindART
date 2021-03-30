55d79a50ac3ed5f7c3f08d39fa5eadc468478343
Muhammad Imam Fernandi <fernandi.bkw@gmail.com>
2021-03-30 23:14:38 +0800

    landing page dan login

---
+++ src/view/auth/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login</title>
+    <link href="../../assets/css/login.css" rel="stylesheet" type="text/css">
+    <link href='//fonts.googleapis.com/css?family=Montserrat:thin,extra-light,light,100,200,300,400,500,600,700,800' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    
+    <div class="container">
+        <img src="../../assets/img/findart_logo.png" alt="logo.png">
+        <h1>FindART</h1>
+
+        <div class="form">
+            <form action="">
+                <label for="username">Username</label><br>
+                <input type="text" id="username" name="username"><br>
+                <label for="password">Password</label><br>
+                <input type="password" name="password" id="password"><br>
+                <button type="submit" name="login">Login</button>
+            </form>
+            <a href="register.php">Belum punya akun?</a>
+        </div>
+        
+    </div>
+
+</body>
+</html>
